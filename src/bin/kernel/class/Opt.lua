53496d96ed7b276235026dd64c63e1025abbb442
John Doe <allyourcoolnamesarebelongtous@gmail.com>
2013-03-19 20:54:47 +0000

    Fixed loadreq crashing when loading modules with errors. Term implemented, not tested.

---
+++ src/bin/kernel/class/Opt.lua
@@ -0,0 +1,25 @@
+class.Opt()
+function Opt:__init(t,typ)
+	self.typ=typ or (typeof(t)=='object' and 'clone' or typeof(t))
+	self.val=t
+end
+function Opt:drop(dst,src)
+	if typ=='raw' then return self.val
+	elseif typ=='clone' then return self.val:clone()
+	elseif typ=='table' then
+		dst=dst or {}
+		src=src or {}
+		for i,v in pairs(self.val) do
+			if ofType(v)==Opt then
+				dst[i]=v:drop(nil,src[i])
+			else
+				if src[i]~=nil then
+					v=src[i]
+				end
+				dst[i]=v
+			end
+		end
+		return dst
+	end	
+end
+return Opt
